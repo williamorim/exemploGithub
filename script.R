0bb93adc8ffae0e43470b3ec6a340ba497cc635a
williamorim <thewilliam89@gmail.com>
2022-11-03 10:34:24 -0300

    coloquei mais instrucoes

--- script.R
+++ script.R
@@ -1,1 +1,11 @@
+# Uma vez por computador
 usethis::use_git_config(user.name = "williamorim", user.email = "thewilliam89@gmail.com")
+usethis::create_github_token()
+# Cria o token
+gitcreds::gitcreds_set()
+
+
+# No início de cada projeto
+usethis::create_project()
+usethis::use_git()
+usethis::use_github()
